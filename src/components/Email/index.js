e892e08a627ad381ca8f75de148453952c3706aa
jbrazjr.es <jbrazjr.es@gmail.com>
2023-01-17 02:12:09 -0300

    chore: style Email reupload

--- src/components/Email/index.js
+++ src/components/Email/index.js
@@ -35,37 +35,39 @@ export default function Email() {
 
  return (
     <div className="container">
+      <div className="area-form">
+        
+        <h1 className="title">Contato</h1>
 
-      <h1 className="title">Contato</h1>
-
-      <form className='form' onSubmit={sendEmail} >
-        <input 
-          className="input"
-          type="text"
-          placeholder="Digite seu nome"
-          onChange={(e) => setName(e.target.value)}
-          value={name}
-        />
+        <form className='form' onSubmit={sendEmail} >
+          <input 
+            className="input"
+            type="text"
+            placeholder="Digite seu nome"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+            
+          <input 
+            className="input"
+            type="text"
+            placeholder="Digite seu email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
           
-        <input 
-          className="input"
-          type="text"
-          placeholder="Digite seu email"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-        />
-         
-         <textarea 
-          className="textarea"
-          placeholder="Digite sua mensagem..."
-          onChange={(e) => setMessage(e.target.value)}
-          value={message}
-        />
+          <textarea 
+            className="textarea"
+            placeholder="Digite sua mensagem..."
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
+          />
 
-        <input className="button" type="submit" value="Enviar" />
+          <input className="button" type="submit" value="Enviar" />
 
-      </form>
+        </form>
 
+      </div>
     </div>
  );
 }
